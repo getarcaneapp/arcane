07ea9b3cceccd2164aac3d5669d290af50518122
Chad Weimer <git@cwmr.dev>
2025-11-30 15:37:25 -0800

    Add a config to the test project to confirm the container properly starts

--- tests/setup/project.data.ts
+++ tests/setup/project.data.ts
@@ -1,14 +1,24 @@
-export const TEST_COMPOSE_YAML = `services:
+export const TEST_COMPOSE_YAML = `configs:
+  some_content:
+    content: |
+      This is a test config file.
+      It can contain multiple lines of text.
+      Used for testing purposes.
+
+services:
   redis:
     image: redis:latest
     container_name: \${CONTAINER_NAME}
+    configs:
+      - source: some_content
+        target: /etc/some_content.txt
+    command: /bin/sh -c 'cat /etc/some_content.txt && redis-server'
     ports:
       - "8081:81"
       - "6379:6379"
       - "6378:6378"
     volumes:
       - redis_data:/data
-    restart: unless-stopped
 
 volumes:
   redis_data:
