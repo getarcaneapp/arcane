bb3204e7dddb16411a7631c216b564e629ed5fe9
ohOgil <clement50260@gmail.com>
2026-08-22 17:57:41 +0200

    fix: consolidate GitOps sync migrations into one file

--- backend/resources/migrations/postgres/075_add_gitops_sync_redeploy.sql
+++
@@ -1,5 +0,0 @@
--- +goose Up
-ALTER TABLE gitops_syncs ADD COLUMN redeploy_after_sync BOOLEAN NOT NULL DEFAULT false;
-
--- +goose Down
-ALTER TABLE gitops_syncs DROP COLUMN IF EXISTS redeploy_after_sync;
--- backend/resources/migrations/sqlite/074_add_gitops_sync_pull_image.sql
+++
@@ -1,5 +0,0 @@
--- +goose Up
-ALTER TABLE gitops_syncs ADD COLUMN pull_image_after_sync BOOLEAN NOT NULL DEFAULT false;
-
--- +goose Down
-ALTER TABLE gitops_syncs DROP COLUMN pull_image_after_sync;
